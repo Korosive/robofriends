484c5bbfe62261e1873e67dd15d423afe3d9ecfc
Korosive <49262788+Korosive@users.noreply.github.com>
2019-12-29 18:45:59 +1100

    Initial project.

---
+++ build/precache-manifest.1fb42592340e1562a584a0bd6917884b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f855ceac9ef1f299c804b98446cda256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "182bab10238a85d3adb0",
+    "url": "/static/css/main.20fcf356.chunk.css"
+  },
+  {
+    "revision": "b8c242a663d4a617c776",
+    "url": "/static/js/2.c7991b8a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c7991b8a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "182bab10238a85d3adb0",
+    "url": "/static/js/main.9de30bfd.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
